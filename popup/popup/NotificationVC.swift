57920de1ff9e5940c33dc0e3ab609065e27ecbc9
honeyvyas <54987463+honeyvyas@users.noreply.github.com>
2019-09-10 13:11:46 +0530

    Add files via upload

---
+++ popup/popup/NotificationVC.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationVC.swift
+//  popup
+//
+//  Created by admin on 14/05/19.
+//  Copyright © 2019 professional. All rights reserved.
+//
+
+import UIKit
+
+class NotificationVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ popup/popup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  popup
+//
+//  Created by admin on 14/05/19.
+//  Copyright © 2019 professional. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
